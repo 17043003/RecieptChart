28f27736d6000680d5d0eabf3f0918948b9b17bc
17043003 <35760051+17043003@users.noreply.github.com>
2022-07-18 04:59:29 +0900

    add: first fragment to show income and expense chart.

--- app/src/main/java/com/ishzk/android/recieptchart/MainActivity.kt
+++ app/src/main/java/com/ishzk/android/recieptchart/MainActivity.kt
@@ -2,10 +2,22 @@ package com.ishzk.android.recieptchart
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import com.ishzk.android.recieptchart.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.materialToolbar)
+
+        val toggle = ActionBarDrawerToggle(
+            this, binding.root, binding.materialToolbar, R.string.app_name, R.string.app_name)
+        binding.root.addDrawerListener(toggle)
+        toggle.syncState()
     }
 }
---
+++ app/src/main/java/com/ishzk/android/recieptchart/fragment/ChartFragment.kt
@@ -0,0 +1,29 @@
+package com.ishzk.android.recieptchart.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.ishzk.android.recieptchart.databinding.FragmentChartBinding
+
+class ChartFragment: Fragment() {
+    private var _binding: FragmentChartBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentChartBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+
+        _binding = null
+    }
+}
