31539a7096c057a1b52ca1f6b622fef03fcb1537
17043003 <35760051+17043003@users.noreply.github.com>
2022-09-16 01:52:33 +0900

    add: show household items on recycler view.

--- app/src/main/java/com/ishzk/android/recieptchart/fragment/HouseholdFragment.kt
+++ app/src/main/java/com/ishzk/android/recieptchart/fragment/HouseholdFragment.kt
@@ -9,9 +9,12 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.findNavController
+import androidx.recyclerview.widget.*
 import com.ishzk.android.recieptchart.R
 import com.ishzk.android.recieptchart.SharedPreference
 import com.ishzk.android.recieptchart.databinding.FragmentHouseholdBinding
+import com.ishzk.android.recieptchart.databinding.ItemRecyclerviewBinding
+import com.ishzk.android.recieptchart.model.Household
 import com.ishzk.android.recieptchart.viewmodel.HouseholdViewModel
 import kotlinx.coroutines.launch
 
@@ -19,6 +22,8 @@ class HouseholdFragment: Fragment() {
     private var _binding: FragmentHouseholdBinding? = null
     private val binding get() = _binding!!
     private val viewModel by viewModels<HouseholdViewModel>()
+
+    private val listAdapter: ListItemAdapter by lazy { ListItemAdapter() }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -35,15 +40,26 @@ class HouseholdFragment: Fragment() {
         viewModel._userID = SharedPreference(requireActivity())
             .getValue(getString(R.string.preference_file_key), getString(R.string.user_id))
 
+        // init recycler view.
+        binding.recyclerView.apply {
+            adapter = listAdapter
+            layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
+
+            val decorator = DividerItemDecoration(context, DividerItemDecoration.VERTICAL)
+            addItemDecoration(decorator)
+        }
+
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        // fetch items on recycler view.
         viewLifecycleOwner.lifecycleScope.launch {
             viewModel.fetchItems().collect{
                 Log.d(TAG, "${it.size}")
+                listAdapter.submitList(it)
             }
         }
     }
@@ -56,5 +72,38 @@ class HouseholdFragment: Fragment() {
 
     companion object {
         const val TAG = "HouseholdFragment"
+    }
+}
+
+class ListItemAdapter: ListAdapter<Household, ItemListViewHolder>(DIFF_UTIL_ITEM_CALLBACK){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemListViewHolder {
+        val view = ItemRecyclerviewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ItemListViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ItemListViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+}
+
+class ItemListViewHolder(private val binding: ItemRecyclerviewBinding):
+    RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: Household){
+            binding.apply {
+                itemDateText.text = item.date.toString()
+                itemCostText.text = item.cost.toString()
+                itemKindText.text = item.kind
+                itemDescriptionText.text = item.description
+            }
+        }
+}
+
+val DIFF_UTIL_ITEM_CALLBACK = object : DiffUtil.ItemCallback<Household>() {
+    override fun areContentsTheSame(oldItem: Household, newItem: Household): Boolean {
+        return oldItem == newItem
+    }
+
+    override fun areItemsTheSame(oldItem: Household, newItem: Household): Boolean {
+        return oldItem.id == newItem.id
     }
 }
--- app/src/main/java/com/ishzk/android/recieptchart/repository/FirestoreRepository.kt
+++ app/src/main/java/com/ishzk/android/recieptchart/repository/FirestoreRepository.kt
@@ -4,6 +4,8 @@ import android.util.Log
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.ishzk.android.recieptchart.model.Household
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import java.time.LocalDateTime
 
 class FirestoreRepository {
@@ -23,39 +25,48 @@ class FirestoreRepository {
             }
     }
 
-    fun fetchItems(userID: String): List<Household> {
+    suspend fun fetchItems(userID: String): List<Household> {
         val itemList = mutableListOf<Household>()
 
         if(userID.isEmpty()) return itemList
 
-        val result = db.collection("users")
-            .document(userID)
-            .collection("items")
-            .get()
-            .addOnSuccessListener { document ->
-                if (document != null) {
-                    Log.d(TAG, "DocumentSnapshot data: ${document.documents}")
-                } else {
-                    Log.d(TAG, "No such document")
+        withContext(Dispatchers.Default) {
+
+            val result = db.collection("users")
+                .document(userID)
+                .collection("items")
+                .get()
+                .addOnSuccessListener { document ->
+                    if (document != null) {
+                        Log.d(TAG, "DocumentSnapshot data: ${document.documents}")
+                    } else {
+                        Log.d(TAG, "No such document")
+                    }
                 }
+                .addOnFailureListener { exception ->
+                    Log.d(TAG, "get failed with ", exception)
+                }
+
+            while (!result.isComplete) {
             }
-            .addOnFailureListener { exception ->
-                Log.d(TAG, "get failed with ", exception)
-            }
 
-        result.result.documents.map {
-            it.data ?: return@map
+            result.result.documents.map {
+                it.data ?: return@map
 
-            val item = Household(
-                it.id,
-                it.data?.get("cost").toString().toIntOrNull() ?: 0,
-                LocalDateTime.now(),
-                it.data?.get("kind").toString(),
-                "",
-                it.data?.get("userID").toString()
-            )
+                val dateHash = it.data?.get("date") as HashMap<*, *>? ?: return@map
+                val year = (dateHash["year"] as Long).toInt()
+                val month = (dateHash["monthValue"] as Long).toInt()
+                val day = (dateHash["dayOfMonth"] as Long).toInt()
+                val date = LocalDateTime.of(year, month, day, 0, 0)
 
-            if(item != null) {
+                val item = Household(
+                    it.id,
+                    it.data?.get("cost").toString().toIntOrNull() ?: 0,
+                    date,
+                    it.data?.get("kind").toString(),
+                    "",
+                    it.data?.get("userID").toString()
+                )
                 itemList.add(item)
             }
         }
